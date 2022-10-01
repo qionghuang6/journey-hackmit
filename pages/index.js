8742c44df1021806834bfbb917a96cd31cd943a3
Qiong Zhou Huang <qiongzhouh@gmail.com>
2022-10-01 15:08:55 -0400

    Add adventure

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import Adventure from '../src/adventure/Adventure';
 
 export default function Index() {
   const [user, setUser] = useState(null)
@@ -16,15 +17,20 @@ export default function Index() {
     getUser()
   }, [])
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="lg">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        <Typography align="center" variant="h4" component="h1" gutterBottom>
           Journey
         </Typography>
-        <Typography variant="h5" component="h1" gutterBottom>
-          User: 
-        </Typography>
-        <Typography>{JSON.stringify(user)}</Typography>
+        <Box sx={{ border: 2, borderColor: 'black', borderRadius: '16px', overflow: 'hidden', padding: 1, background: 'linear-gradient(270deg, rgb(180, 160, 0), rgb(220, 73, 140))'}}>
+          <Typography color="white" variant="h3" component="h1" gutterBottom>
+            User: 
+          </Typography>
+          <Typography color="white" variant="h4">{JSON.stringify(user)}</Typography>
+        </Box>
+        <Adventure />
+        <Adventure />
+        <Adventure />
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
