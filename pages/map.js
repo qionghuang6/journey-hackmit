35b6c0ba1d55813e3d887f6da01049db7ce05baf
Philena Liu <philenaliu@Philenas-MacBook-Pro.local>
2022-10-02 07:34:08 -0400

    created navbar

--- pages/map.js
+++ pages/map.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import getApiUrl from '../src/getApiUrl';
 import MapContainer from '../components/MapContainer';
+import NavBar from '../components/NavBar';
 
 export default function map() {
 
@@ -8,6 +9,7 @@ export default function map() {
         <>
         
         <MapContainer/>
+        <NavBar/>
         </>
     )
 }
