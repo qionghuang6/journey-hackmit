70a76ea8615fb5fa58fae50ef55023357480b81f
Philena Liu <philenaliu@Philenas-MacBook-Pro.local>
2022-10-01 21:00:27 -0400

    worked on map fetching stuff

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -1,37 +1,53 @@
-import React, {Component} from 'react'
+import { useEffect, useState } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'                         
 import mapStyles from '../src/mapStyles'
-class MapContainer extends Component{
-    state = {myMarkers: [
-    {latitude: 40.710992, longitude: -74.008292},   
-    {latitude: 40.792917, longitude: -73.969497},
-    {latitude:  40.710992, longitude: -74.008292}]
-    }
-    displayMarkers = () => {                                        
-                return this.state.myMarkers.map((mark, index) => {                
-                    return <Marker id={index}  position={{                            
+import getApiUrl from '../src/getApiUrl';
+function MapContainer(props) {
+    const [myMarkers, setMyMarkers] = useState([
+        {id: 1, latitude: 40.710992, longitude: -74.008292},   
+        {id: 2, latitude: 40.792917, longitude: -73.969497},
+        {id: 3, latitude:  40.710992, longitude: -74.008292}
+    ])
+    useEffect(() => {
+        const query = { users: ['test'], location: [40.2, -75], radius: 1, tag: 'restaurant' }
+        const getExperiences = async () => {
+          const res = await fetch(
+            getApiUrl(`/api/experiences/radar?${new URLSearchParams(query)}`), 
+            { method: 'GET' }
+          )
+        //   const result = await res.json()
+        //   setMyMarkers(result)
+          console.log("RESULTT", res)
+        }
+        getExperiences()
+      }, [])
+    
+    function displayMarkers() {                                        
+                return myMarkers.map((mark, index) => {                
+                    return <Marker id={mark.id}  position={{                            
                         lat: mark.latitude,                                              
                         lng: mark.longitude                                                
                     }} 
-            onClick={() => console.log("You clicked me!",{index})} />          
+            onClick={() => {
+                
+            }} />          
         })
     }
-    render() {
         return (
             <div style={{
             position: "relative",
             width: "100%",
             height: "100vh"}} 
             className="map">
-            <Map google={this.props.google} 
+            <Map google={props.google} 
             zoom={13}
             styles={mapStyles.styles}
             initialCenter={{ lat: 40.7812, lng: -73.9665}}
             disableDefaultUI= {true}>
-            {this.displayMarkers()}</Map>
+            {displayMarkers()}</Map>
             </div>
             );
-}}
+}
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyCdEKu3k2avk5Y5Ru2EGSGzyyrAm2UcLpU'
     })(MapContainer)
--- pages/map.js
+++ pages/map.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import getApiUrl from '../src/getApiUrl';
 import MapContainer from '../components/MapContainer';
 
-export default function Index() {
+export default function map() {
 
     return (
         <>
